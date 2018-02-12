b4a2dcaeece4b3e14ab8d22f332e665c92a7bb50
Imre Bartis <imre.bartis@siili.com>
2018-02-12 08:56:55 +0200

    code cleanup + injecting the Book model

--- app.js
+++ app.js
@@ -7,48 +7,15 @@ var db = mongoose.connection.openUri('mongodb://localhost/bookAPI');
 var Book = require('./models/bookModel');
 
 var app = express();
-var port = process.env.PORT || 3000;
 
-var bookRouter = express.Router();
+var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-bookRouter.route('/books')
-    .post(function(req, res){
-       var book = new Book(req.body);
-       book.save();
-       res.status(201).send(book) // 201 = created
+bookRouter = require('./Routes/bookRoutes')(Book);
 
-    })
-    .get(function(req, res){
-
-        var query = {};
-
-        if(req.query.genre){
-            query.genre = req.query.genre
-        };
-
-        Book.find(query, function(err, books){
-            if(err) 
-            res.status(500).send(err);
-            else 
-                res.json(books)  
-        })
-    });
-
-bookRouter.route('/books/:bookId')
-    .get(function(req, res){
-
-        Book.findById(req.params.bookId, function(err, book){
-            if(err) 
-            res.status(500).send(err);
-            else 
-                res.json(book)  
-        })
-    });
-
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
 
 app.get('/', function (req, res) {
     res.send('Welcome to my API');
