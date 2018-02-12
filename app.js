4836f5bffb9cd71f788d7bec38c79070b9291307
Imre Bartis <imre.bartis@siili.com>
2018-02-12 12:31:07 +0200

    integration tests with SuperTest

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ var express = require('express'),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
 
-var db = mongoose.connection.openUri('mongodb://localhost/bookAPI');
+var db;
 
+if(process.env.ENV === 'Test') 
+    db = mongoose.connection.openUri('mongodb://localhost/bookAPI_test');
+else {
+    db = mongoose.connection.openUri('mongodb://localhost/bookAPI');
+}
 var Book = require('./models/bookModel');
 
 var app = express();
